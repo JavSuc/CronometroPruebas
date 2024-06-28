d5932da56bf76cb09b5cf82ce96a265c0d07aea3
JavSuc <jsucunza@ibecapture.com>
2024-06-28 11:48:11 +0200

    Version definitiva cronometro simple

--- CronometroPruebas/MainWindow.xaml.cs
+++ CronometroPruebas/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace CronometroPruebas
 {
@@ -16,9 +17,36 @@ namespace CronometroPruebas
     /// </summary>
     public partial class MainWindow : Window
     {
+        int Segundos = 0;
+        DispatcherTimer dt = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
+            dt.Interval = TimeSpan.FromSeconds(1);
+            dt.Tick += tickevent;
+            txtSegundos.Text = Segundos.ToString();
+        }
+
+        private void btnArrancar_Click(object sender, RoutedEventArgs e)
+        {
+            dt.Start();
+        }
+
+        private void tickevent(object sender, EventArgs e)
+        {
+            Segundos++;
+            txtSegundos.Text = Segundos.ToString();
+        }
+        private void btnPausar_Click(object sender, RoutedEventArgs e)
+        {
+            dt.Stop();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            Segundos = 0;
+            txtSegundos.Text = Segundos.ToString();
+            dt.Stop();
         }
     }
 }
